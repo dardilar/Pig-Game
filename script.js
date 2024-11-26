271eb83227d399f7f8d7667e5e4294353a0ab0b3
dsardila <dsardila1809@hotmail.com>
2024-11-25 22:00:14 -0500

    Adding relative path

--- script.js
+++ script.js
@@ -50,7 +50,7 @@ btnRoll.addEventListener('click', function () {
 
     // 2. Display dice
     diceEl.classList.remove('hidden');
-    diceEl.src = `/img/dice-${dice}.png`;
+    diceEl.src = `img/dice-${dice}.png`;
 
     // 3. Check for rolled 1
     if (dice !== 1) {
